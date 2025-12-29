1840c75617b274ff2f1c0b46781684e25622e326
Ameenullah Kamangar <ameenkamangar@gmail.com>
2025-12-30 00:13:14 +0500

    Add files via upload

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './brand.css';
+import { google, atlassian, shopify, dropbox, slack } from './imports';
+const Brand = () => {
+    return (
+        <div className="Brand Section_Padding">
+
+            <div>
+                <img src={google} alt="google" />
+            </div>
+
+            <div>
+                <img src={slack} alt="slack" />
+            </div>
+
+            <div>
+                <img src={atlassian} alt="atlassian" />
+            </div>
+
+            <div>
+                <img src={shopify} alt="shopify" />
+            </div>
+
+            <div>
+                <img src={dropbox} alt="dropbox" />
+            </div>
+        </div>
+    )
+}
+
+export default Brand
