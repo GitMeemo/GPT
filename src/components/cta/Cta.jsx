8f2e1c1e8d8104d2c3881c53e7b4bc518bf33b01
Ameenullah Kamangar <ameenkamangar@gmail.com>
2026-01-04 01:48:32 +0500

    Add files via upload

---
+++ src/components/cta/Cta.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './cta.css';
+const Cta = () => {
+  return (
+    <div className="CTA">
+      <div className="CTA-Content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+
+      <div className="CTA-Btn">
+        <button>Get Started</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Cta
